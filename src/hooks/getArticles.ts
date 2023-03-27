df436f9b912c2f482f83021be75bbb0b1987a2e7
Zach Killebrew <zbkilleb@gmail.com>
2023-03-27 08:31:24 -0500

    Refactor getArticles algorithm

--- src/hooks/getArticles.ts
+++ src/hooks/getArticles.ts
@@ -5,16 +5,12 @@ import { getPreferences } from './getPreferences';
 
 export async function getArticles() {
   const preferences = getPreferences();
-  const articleArr = [];
 
-  for (let i = 0; i < preferences.length; i++) {
-    const result = await getFeed(preferences[i]);
-    articleArr.push(...result);
-  }
-
-  return articleArr.sort((a: Item, b: Item) => {
-    if (b.pubDate && a.pubDate)
-      return dayjs(b.pubDate).unix() - dayjs(a.pubDate).unix();
-    return 0;
+  return Promise.all(preferences.map(getFeed)).then((data) => {
+    return data.flat().sort((a: Item, b: Item) => {
+      if (b.pubDate && a.pubDate)
+        return dayjs(b.pubDate).unix() - dayjs(a.pubDate).unix();
+      return 0;
+    });
   });
 }
